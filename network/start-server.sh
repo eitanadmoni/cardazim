c436f295cbf5b837a0b23ffa98ceaec7e7dab201
Eitan Admoni <eitanadmoni@mail.tau.ac.il>
2024-07-01 16:02:07 +0300

    a

--- network/start-server.sh
+++ network/start-server.sh
@@ -1,5 +1,9 @@
+echo [SERVER] Installing dependencies…
 pip3 install flask
 pip3 install pillow
 pip3 install PyCryptodome
 pip3 install sqlite3
+echo [SERVER] Done installing dependencies…
+echo [SERVER] Starting server...
 python3 cards_api.py 127.0.0.1 5000 sql://database.db
+echo [SERVER] Server running!
